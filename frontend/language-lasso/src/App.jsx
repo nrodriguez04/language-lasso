30b1b7a53c41b023ed3366e2350e2335be16b1dc
nrodriguez04 <nicolasmrodriguez3@gmail.com>
2023-06-08 18:17:29 -0500

    Initial Frontend Commit

--- frontend/language-lasso/src/App.jsx
+++ frontend/language-lasso/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState('');
+  const [translation, setTranslation] = useState('');
+
+  const translateText = async () => {
+    try {
+      const response = await axios.post('http://localhost:5000/translate', { text });
+      setTranslation(response.data.translated_text);
+    } catch (error) {
+      console.error('Failed to translate text:', error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <textarea
+        value={text}
+        onChange={event => setText(event.target.value)}
+      />
+      <button onClick={translateText}>
+        Translate
+      </button>
+      {translation && <p>{translation}</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
+
